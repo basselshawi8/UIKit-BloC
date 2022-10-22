09d21d11acc6b5dc943d2ab05d74ee517a85d6f6
tarek.murad <bassel.shawi@gmail.com>
2022-10-22 15:58:56 +0300

    Initial Commit

---
+++ Bloc/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Bloc
+//
+//  Created by iMac on 10/22/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
